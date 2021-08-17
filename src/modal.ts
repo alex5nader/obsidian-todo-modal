13e03a4048c16222d31a7d09c15ff4b899acbfb0
Alex Habich <contact@alexhabi.ch>
2021-08-16 19:45:54 -0500

    Implement date parsing

--- src/modal.ts
+++ src/modal.ts
@@ -1,5 +1,20 @@
-import {App, Modal, normalizePath, Setting} from "obsidian";
+import {App, Modal, moment, normalizePath, Setting} from "obsidian";
 import TodoModalPlugin from "./main";
+
+import nlp from "compromise";
+import nlpNumbers from "compromise-numbers";
+import nlpDates from "compromise-dates";
+//@ts-ignore
+import nlpKeypress from "compromise-keypress";
+
+nlp.extend(nlpNumbers);
+nlp.extend(nlpDates)
+nlp.extend(nlpKeypress);
+
+type DateRangeSelector = "start" | "end";
+type DateRange = {
+    [selector in DateRangeSelector]: string;
+};
 
 export class CreateTodoModal extends Modal {
     plugin: TodoModalPlugin;
@@ -46,15 +61,50 @@ export class CreateTodoModal extends Modal {
                     drop.onChange(value => priority = value);
                 });
 
-            let dueDate = "";
+            let rangeSelector: DateRangeSelector = "start";
+            //@ts-ignore // plugins dont work nice with typescript
+            let dueDate: DateRange | undefined = undefined;
             let dueDatePreview: HTMLElement;
+            function updatePreview() {
+                console.log(dueDate);
+                dueDatePreview.setText(moment(dueDate[rangeSelector]).format("YYYY-MM-DD, HH:mm"));
+            }
+
+            new Setting(form)
+                .setName("Start or end?")
+                .addToggle(toggle => {
+                    toggle.toggleEl.classList.add("tmp-labelled-toggle");
+
+                    toggle.setValue(false);
+                    toggle.onChange(value => {
+                        if (value) {
+                            rangeSelector = "end";
+                        } else {
+                            rangeSelector = "start";
+                        }
+                        updatePreview();
+                    });
+
+                    const parent = toggle.toggleEl.parentElement;
+                    const startEl = parent.createEl("span", {text: "start"});
+                    const endEl = parent.createEl("span", {text: "end"});
+                    parent.setChildrenInPlace([startEl, toggle.toggleEl, endEl]);
+                });
+
+            let dueDateText: string = "";
             const dueDateInput = new Setting(form)
                 .setName("Due By")
                 .addText(text => {
                     text.onChange(value => {
-                        dueDate = value;
+                        dueDateText = value;
+                        //@ts-ignore // plugins dont work nice with typescript
+                        dueDate = nlp(dueDateText).dates().json(0);
 
-                        dueDatePreview.setText(dueDate);
+                        if (!dueDate) {
+                            dueDatePreview.setText("Invalid");
+                        } else {
+                            updatePreview();
+                        }
                     });
                 });
             dueDatePreview = dueDateInput.descEl;
@@ -72,15 +122,21 @@ export class CreateTodoModal extends Modal {
 
                 this.close();
 
-                const content =
-`---
-done: false
-group: ${this.plugin.settings.groups[Number.parseInt(group)]}
-priority: ${this.plugin.settings.priorities[Number.parseInt(priority)]}
-due-by: ${dueDate}
----
+                let dueBy: string | undefined;
+                if (dueDate) {
+                    dueBy = moment(dueDate[rangeSelector]).format("YYYY-MM-DD[T]HH:mm:ss");
+                } else if (dueDateText) {
+                    dueBy = dueDateText;
+                } else {
+                    dueBy = undefined;
+                }
 
-`
+                const content = new FrontMatterBuilder()
+                    .add("done", "false")
+                    .add("group", this.plugin.settings.groups[Number.parseInt(group)])
+                    .add("priority", this.plugin.settings.priorities[Number.parseInt(priority)])
+                    .add("due-by", dueBy)
+                    .build() + "\n\n";
 
                 const path = normalizePath(this.plugin.settings.todoPath + "/" + title + ".md");
                 console.log(path);
@@ -95,5 +151,33 @@ due-by: ${dueDate}
                 });
             })
         });
+    }
+}
+
+class FrontMatterBuilder {
+    entries: Map<string, string> = new Map<string, string>()
+
+    add(key: string, value: string | undefined): this {
+        if (!value) {
+            return this;
+        }
+
+        this.entries.set(key, value);
+
+        return this;
+    }
+
+    build(): string {
+        if (this.entries.size == 0) {
+            return "";
+        }
+
+        let result = "---";
+
+        for (const [key, value] of this.entries) {
+            result += `\n${key}: ${value}`
+        }
+
+        return result + "\n---";
     }
 }
